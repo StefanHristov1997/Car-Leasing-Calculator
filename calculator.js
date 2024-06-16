1755db5746c065056ecb9abb3a62fad237bd55be
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-06-16 13:40:31 +0300

    Add js and event listener for the document

---
+++ calculator.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const carValueInput = document.getElementById('car-value');
+    const carValueSlider = document.getElementById('car-value-slider');
+    const downPaymentInput = document.getElementById('down-payment');
+    const downPaymentSlider = document.getElementById('down-payment-slider');
+    const leasePeriodSelect = document.getElementById('lease-period');
+    const carTypeSelect = document.getElementById('car-type');
+
+    const interestRates = {
+        'brand-new': 0.0299,
+        'used': 0.037
+    };
+};
+
